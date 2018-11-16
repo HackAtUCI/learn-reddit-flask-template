3a511379d76391341136a62cb06d7e85372e35a1
raopg <prateek.rao.08@gmail.com>
2018-11-15 17:56:22 -0800

    Numbered TODOs

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #TODO: Assign a Reddit subreddit name to use in this function.
+    #TODO: (13) Assign a Reddit subreddit name to use in this function.
     subreddit_name = '' #Assign this variable to your favorite subreddit.
-    #TODO: Create an instance of the praw Reddit object. Refer to reddit.py
-    #TODO: Generate a list of top ten posts for your subreddit.
-    #TODO: Pass the list you created above to the render_template function call below.
+    #TODO: (14) Create an instance of the praw Reddit object. Refer to reddit.py
+    #TODO: (15) Generate a list of top ten posts for your subreddit.
+    #TODO: (16) Pass the list you created above to the render_template function call below.
     return render_template('index.html',posts = ,name = )
 
 if __name__ == "__main__":
--- reddit.py
+++ reddit.py
@@ -5,6 +5,10 @@ from urllib import request
 '''The reddit_instance object will contain the reddit instance we will create to handle all API calls. 
    Read the Python Reddit API Wrapper(PRAW) documentation for further details'''
 
+#TODO: (1) Create a Reddit app at https://reddit.com/prefs/apps
+#TODO: (2) Update your credentials in the credentials_template.json file
+#TODO: (3) Rename credentials_template.json to credentials.json
+#Tip: bash command for above step: cp credentials_template.json credentials.json
 
 def create_reddit_instance(read_only = False):
     '''This function reads the crendentials from credentials.json and creates a Reddit instance.
@@ -13,17 +17,17 @@ def create_reddit_instance(read_only = False):
         credentials_template.json and rename it to credentials.json
     '''
     credentials = None
-    #TODO: Open 'credentials.json' and read the information into the credentials object initialized above. 
+    #TODO: (4) Open 'credentials.json' and read the information into the credentials object initialized above. 
     
     '''The read only parameter determines whether a read-only object,i.e, no login required, 
     or an OAuth2 authenticated object should be created
     '''
-    #TODO: Pass all parameters relevant to read-only and user-authenticated objects. One of the parameters is already completed for you.
+    #TODO: (5) Pass all parameters relevant to read-only and user-authenticated objects. One of the parameters is already completed for you.
     if(not read_only):
         reddit_instance = praw.Reddit(client_id = credentials['client_id'],)
     else:
         reddit_instance = praw.Reddit()
-    #TODO: Return the reddit instance you created below.
+    #TODO: (6) Return the reddit instance you created below.
     return 
     
     
@@ -32,19 +36,19 @@ def ten_top_posts(reddit_instance, subreddit_name):
     under the top category'''
     #TODO: Create a subreddit instance with subreddit_name and reddit_instance.
 
-    #TODO: Return the top posts in the subreddit by calling the .top() function on it.
-    #TODO: Set the limit parameter for the top function to 10, so that the top 10 posts are returned.
+    #TODO: (7) Return the top posts in the subreddit by calling the .top() function on it.
+    #TODO: (8) Set the limit parameter for the top function to 10, so that the top 10 posts are returned.
     return 
 
 if __name__ == '__main__':
     '''Experiment with your reddit function here. Examine the output on the console.'''
-    #TODO: Set the value of subreddit_name to your favorite subreddit.
+    #TODO: (9) Set the value of subreddit_name to your favorite subreddit.
     subreddit_name = '' 
-    #TODO: Create a new instance of the praw Reddit object.
+    #TODO: (10) Create a new instance of the praw Reddit object.
 
-    #TODO: Generate a list of top ten posts for your subreddit, using the praw Reddit object you created.
+    #TODO: (11) Generate a list of top ten posts for your subreddit, using the praw Reddit object you created.
 
-    #TODO: Iterate through the posts list returned by the function call above, and print its title attribute
+    #TODO: (12) Iterate through the posts list returned by the function call above, and print its title attribute
     for():
         pass
 
