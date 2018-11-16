696e030905a01d913f4cf6cb65ffcef93bde9515
raopg <prateek.rao.08@gmail.com>
2018-11-15 22:41:20 -0800

    Updated TODO count, templating syntax

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #TODO: (13) Assign a Reddit subreddit name to use in this function.
+    #TODO: (14) Assign a Reddit subreddit name to use in this function.
     subreddit_name = '' #Assign this variable to your favorite subreddit.
-    #TODO: (14) Create an instance of the praw Reddit object. Refer to reddit.py
-    #TODO: (15) Generate a list of top ten posts for your subreddit.
-    #TODO: (16) Pass the list you created above to the render_template function call below.
+    #TODO: (15) Create an instance of the praw Reddit object. Refer to reddit.py
+    #TODO: (16) Generate a list of top ten posts for your subreddit.
+    #TODO: (17) Pass the list you created above to the render_template function call below.
     return render_template('index.html',posts = ,name = )
 
 if __name__ == "__main__":
--- reddit.py
+++ reddit.py
@@ -34,22 +34,22 @@ def create_reddit_instance(read_only = False):
 def ten_top_posts(reddit_instance, subreddit_name):
     '''This function takes a subreddit name as a string and prints out ten posts
     under the top category'''
-    #TODO: Create a subreddit instance with subreddit_name and reddit_instance.
+    #TODO: (7) Create a subreddit instance with subreddit_name and reddit_instance.
 
-    #TODO: (7) Return the top posts in the subreddit by calling the .top() function on it.
-    #TODO: (8) Set the limit parameter for the top function to 10, so that the top 10 posts are returned.
+    #TODO: (8) Return the top posts in the subreddit by calling the .top() function on it.
+    #TODO: (9) Set the limit parameter for the top function to 10, so that the top 10 posts are returned.
     return 
 
 if __name__ == '__main__':
     '''Experiment with your reddit function here. Examine the output on the console.'''
-    #TODO: (9) Set the value of subreddit_name to your favorite subreddit.
+    #TODO: (10) Set the value of subreddit_name to your favorite subreddit.
     subreddit_name = '' 
-    #TODO: (10) Create a new instance of the praw Reddit object.
+    #TODO: (11) Create a new instance of the praw Reddit object.
 
-    #TODO: (11) Generate a list of top ten posts for your subreddit, using the praw Reddit object you created.
+    #TODO: (12) Generate a list of top ten posts for your subreddit, using the praw Reddit object you created.
 
-    #TODO: (12) Iterate through the posts list returned by the function call above, and print its title attribute
-    for():
+    #TODO: (13) Iterate through the posts list returned by the function call above, and print its title attribute
+    for:
         pass
 
 
